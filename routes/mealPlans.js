997f93720be9138aabc341bc9456dd572a19f3b2
KristinLind <kristin.lind@comcast.net>
2026-03-24 20:39:19 -0600

    Add swagger comments for mealPlans

--- routes/mealPlans.js
+++ routes/mealPlans.js
@@ -9,10 +9,45 @@ import {
 
 const router = express.Router();
 
+/**
+ * #swagger.tags = ['MealPlans']
+ * #swagger.summary = 'Get all meal plans'
+ */
 router.get("/", getAllMealPlans);
+
+/**
+ * #swagger.tags = ['MealPlans']
+ * #swagger.summary = 'Get a single meal plan by ID'
+ */
 router.get("/:id", getSingleMealPlan);
+
+/**
+ * #swagger.tags = ['MealPlans']
+ * #swagger.summary = 'Create a new meal plan'
+ * #swagger.parameters['body'] = {
+ *   in: 'body',
+ *   description: 'Meal plan object',
+ *   required: true,
+ *   schema: {
+        "day": "Monday",
+        "mealType": "Dinner",
+        "recipeId": "ObjectId",
+        "notes": "Family favorite"
+      }
+ * }
+ */
 router.post("/", createMealPlan);
+
+/**
+ * #swagger.tags = ['MealPlans']
+ * #swagger.summary = 'Update a meal plan'
+ */
 router.put("/:id", updateMealPlan);
+
+/**
+ * #swagger.tags = ['MealPlans']
+ * #swagger.summary = 'Delete a meal plan'
+ */
 router.delete("/:id", deleteMealPlan);
 
 export default router;
