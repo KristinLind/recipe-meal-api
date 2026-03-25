fa31d886e17dd5d86153b2d2e09a2e1fe318e29d
KristinLind <kristin.lind@comcast.net>
2026-03-25 08:30:19 -0600

    Fix swagger body parsing

--- swagger.js
+++ swagger.js
@@ -1,20 +1,20 @@
 import swaggerAutogen from "swagger-autogen";
 
-const swagger = swaggerAutogen();
+const swagger = swaggerAutogen({ openapi: "3.0.0" });
 
 const doc = {
   info: {
     title: "Recipe API",
-    description: "API documentation for recipes and meal plans"
+    description: "API documentation for recipes and meal plans",
+    version: "1.0.0"
   },
   host: "cse341-node-r9gw.onrender.com",
-  schemes: ["https"],
-  basePath: "/"
+  schemes: ["https"]
 };
 
 const outputFile = "./swagger.json";
+
 const endpointsFiles = [
-  "./server.js",
   "./routes/recipes.js",
   "./routes/mealPlans.js"
 ];
